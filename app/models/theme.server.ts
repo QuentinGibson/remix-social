bb4ae38f53b7bc2e91e997fc3d10dda968667fdd
Quentin Gibson <quentingibson94@gmail.com>
2023-03-28 17:09:19 -0400

    add theme

--- app/models/theme.server.ts
+++ app/models/theme.server.ts
@@ -13,6 +13,19 @@ export async function createDefaultTheme() {
   });
 }
 
+export async function populateTheme() {
+  await prisma.theme.create({
+    data: {
+      name: "dark",
+      primary: "hsl(200, 16%, 19%)",
+      secondary: "hsl(218, 18%, 32%)",
+      accent: "hsl(251,80%,34%)",
+      accent2: "hsl(283,90%,32%)",
+      mood: "dark",
+    },
+  });
+}
+
 export async function getTheme(userId: string) {
   const user = await prisma.user.findUnique({
     where: { id: userId },
